a681669e63f4ccae5f1f11e190f413cd7a277782
Toni Hermoso Pulido <toni.hermoso@crg.eu>
2026-03-27 15:03:54 +0100

    adding schema.org

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,9 @@ project = "Sphinx and embedding Bioschemas"
 copyright = "2026, Centre for Genomic Regulation (CRG)"
 author = "Toni Hermoso Pulido"
 
-version = "2026"
+html_title = "Introduction to Sphinx - embedding schemas.org/Bioschemas metadata"
+
+version = "2026.04.01"
 release = version
 
 # -- Matomo configuration ---------------------------------------------------
@@ -32,27 +34,8 @@ html_context = {
     "matomo_site_id": matomo_site_id,
 }
 
-# Handling bioschemas
-def convert_dates(obj):
-    if isinstance(obj, dict):
-        return {k: convert_dates(v) for k, v in obj.items()}
-    elif isinstance(obj, list):
-        return [convert_dates(i) for i in obj]
-    elif isinstance(obj, (datetime.date, datetime.datetime)):
-        return obj.isoformat()
-    else:
-        return obj
 
-
-bioschemas_jsonld = None
-yaml_path = os.path.join(os.path.dirname(__file__), "bioschemas.yaml")
-if os.path.exists(yaml_path):
-    with open(yaml_path, "r") as f:
-        bioschemas_data = yaml.safe_load(f)
-    bioschemas_data = convert_dates(bioschemas_data)
-    bioschemas_jsonld = json.dumps(bioschemas_data, indent=2)
-    html_context["bioschemas_jsonld"] = bioschemas_jsonld
-
+bioschemas = ["schemaorg.yaml"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
